812e3e4938fc0060d4ee2afbace92b5a30073335
Matias Arias <matiasarias3103@gmail.com>
2026-05-27 17:26:48 -0400

    Agregando gif boton especial

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 const letters = document.querySelectorAll(".letter");
+const especialBtn = document.querySelector("#filler-gif");
 
 letters.forEach((element) => {
   const letter = element.textContent.trim();
+  console.log(letter);
 
   const img = document.createElement("img");
-  img.src = `img/${letter}.jpeg`;
+
+  if (letter == "???") {
+    img.src = `img/si.gif`;
+    console.log(img);
+  } else {
+    img.src = `img/${letter}.jpeg`;
+  }
 
   img.classList.add("letter-img");
 
